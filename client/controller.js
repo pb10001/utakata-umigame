868779dc3d7f9beef6df92d9bb9d10942b8a409a
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2017-12-06 15:17:20 +0900

    Connect to Redis

--- client/controller.js
+++ client/controller.js
@@ -176,20 +176,5 @@ app.config(['$routeProvider','$locationProvider', function($routeProvider, $loca
     }
   };
 }).controller('RedisClient', function redisClient($scope){
-    //参考: https://qiita.com/5a3i/items/224ee1ea234d90d9dd7a
-    var redis = require("redis"),
-    url   = require("url")
 
-    if (process.env.REDISTOGO_URL) {
-        var rtg    = url.parse(process.env.REDISTOGO_URL);
-        var client = redis.createClient(rtg.port, rtg.hostname);
-
-        client.auth(rtg.auth.split(":")[1]);
-    } else {
-        var client = redis.createClient();
-    }
-
-    client.on("error", function (err) {
-        console.log("Error " + err);
-    });
 });
--- server.js
+++ server.js
@@ -10,6 +10,20 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
+var redis = require('redis');
+var url   = require('url');
+//参考: https://qiita.com/5a3i/items/224ee1ea234d90d9dd7a
+if (process.env.REDISTOGO_URL) {
+    var rtg    = url.parse(process.env.REDISTOGO_URL);
+    var client = redis.createClient(rtg.port, rtg.hostname);
+
+    client.auth(rtg.auth.split(":")[1]);
+} else {
+    var client = redis.createClient();
+}
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 //
 // ## SimpleServer `SimpleServer(obj)`
 //
@@ -32,7 +46,6 @@ chat=io.on('connection', function (socket) {
     user_id+=1;
     socket.user_id=user_id;
     sockets.push(socket);
-    
     socket.on('join', function(roomId){
         socket.leave(socket.room);
         socket.room=roomId;
