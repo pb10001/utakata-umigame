f6b11c691e33faeed49f55c7fe07416c4771f392
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2017-12-08 22:13:15 +0900

    Add navbar with sidebar

--- server.js
+++ server.js
@@ -10,21 +10,6 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
-var redis = require('redis');
-var url   = require('url');
-//参考: https://qiita.com/5a3i/items/224ee1ea234d90d9dd7a
-if (process.env.REDISTOGO_URL) {
-    var rtg    = url.parse(process.env.REDISTOGO_URL);
-    var client = redis.createClient(rtg.port, rtg.hostname);
-
-    client.auth(rtg.auth.split(":")[1]);
-} else {
-    var client = redis.createClient();
-}
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
-
 //
 // ## SimpleServer `SimpleServer(obj)`
 //
