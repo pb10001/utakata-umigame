6dd2d187a7975a09d4d20d31e31c11002f9d3a35
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-01-19 16:32:45 +0900

    Modularize js

---
+++ src/angular-init.js
@@ -0,0 +1,30 @@
+var $ = require('jquery');
+window.jQuery = $;var bootstrap = require('bootstrap');
+var angular = require('angular');
+var ngRoute = require('angular-route');
+
+var app = angular.module('App', ['ngRoute']);
+app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode({
+    enabled:true,
+    requireBase: false    
+  });
+  $routeProvider
+    .when('/',{
+        templateUrl:'/top_page_beta.html',
+        controller:''
+    })
+    .when('/mondai/:room',{
+        templateUrl : '/mondai.html',
+        controller : 'ChatController'
+    })
+    .when('/privacy_policy',{
+        templateUrl: 'privacy_policy.html',
+        controller:''
+    })
+	.otherwise({
+		redirectTo: '/'
+	});
+  }]);
+  
+module.exports = app;
