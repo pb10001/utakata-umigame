35847122663fca6fef6f51351cb90a8c0594dc48
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-01-19 17:45:04 +0900

    Add link to "Public" room

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ var chatController = require('./chat');
 function enterController(){
     this.roomName = '';
 }
+function publicController(){
+  this.roomName = 'Public';
+}
 app.component('enter',{
-	template: ['<div class="input-append">',
+	template: ['<h3 class="sawarabi">チャットをはじめる</h3>',
+        '<div class="input-append">',
 			  '<input ng-model="$ctrl.roomName" class="form-control" placeholder="Room Name">',
 			  '<a class="btn btn-default" ng-href="/mondai/{{$ctrl.roomName}}">新規作成/入室</a>',
 		      '</div>'].join(''),
   controller: enterController
 });
+app.component('public',{
+  template:'<a class="btn btn-default" ng-href="/mondai/{{$ctrl.roomName}}">Publicルームへ</a>',
+  controller: publicController
+})
 app.controller('ChatController', chatController);
