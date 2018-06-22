157c1bb18b01a4f7c4807467a8d0dc851548c9c5
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-06-22 04:11:20 +0000

    Add babel.js and .babelrc

---
+++ babel.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var browserify = require("browserify");
+var babelify = require("babelify");
+var uglifyjs = require("uglify-js");
+ 
+//bundle
+var bundle = {
+  js: './client/js/bundle.js',
+  min: './client/js/bundle.min.js'
+};
+browserify({ debug: true })
+  .transform(babelify)
+  .require("./src/main.js", { entry: true })
+  .bundle()
+  .on("error", function (err) { console.log("Error: " + err.message); })
+  .pipe(fs.createWriteStream(bundle.js));
+  
+//top
+var top = {
+  js: './client/js/top.js',
+  min: './client/js/top.min.js'
+}
+browserify({ debug: true })
+  .transform(babelify)
+  .require("./src/top.js", { entry: true })
+  .bundle()
+  .on("error", function (err) { console.log("Error: " + err.message); })
+  .pipe(fs.createWriteStream(top.js));
