5f8c40a34e2db3452c31f938f3a7ff2bfc94756a
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-06-22 06:02:08 +0000

    Update package.json

--- babel.js
+++ babel.js
@@ -1,8 +1,8 @@
-var fs = require("fs");
-var browserify = require("browserify");
-var babelify = require("babelify");
-var uglifyjs = require("uglify-js");
- 
+var fs = require('fs');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var uglifyjs = require('uglify-js');
+
 //bundle
 var bundle = {
   js: './client/js/bundle.js',
@@ -10,19 +10,23 @@ var bundle = {
 };
 browserify({ debug: true })
   .transform(babelify)
-  .require("./src/main.js", { entry: true })
+  .require('./src/main.js', { entry: true })
   .bundle()
-  .on("error", function (err) { console.log("Error: " + err.message); })
+  .on('error', function(err) {
+    console.log('Error: ' + err.message);
+  })
   .pipe(fs.createWriteStream(bundle.js));
-  
+
 //top
 var top = {
   js: './client/js/top.js',
   min: './client/js/top.min.js'
-}
+};
 browserify({ debug: true })
   .transform(babelify)
-  .require("./src/top.js", { entry: true })
+  .require('./src/top.js', { entry: true })
   .bundle()
-  .on("error", function (err) { console.log("Error: " + err.message); })
+  .on('error', function(err) {
+    console.log('Error: ' + err.message);
+  })
   .pipe(fs.createWriteStream(top.js));
