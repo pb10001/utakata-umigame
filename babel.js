dc4a965cff8f5c4c7ad72952e65d7dc5684983f7
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-06-25 13:08:49 +0900

    Employ watcify

--- babel.js
+++ babel.js
@@ -2,14 +2,20 @@ var fs = require('fs');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var uglifyjs = require('uglify-js');
+var watchify = require('watchify');
 
 //bundle
 var bundle = {
   js: './client/js/bundle.js',
   min: './client/js/bundle.min.js'
 };
-browserify({ debug: true })
-  .transform(babelify)
+var b = browserify({
+  debug: true,
+  cache: {},
+  packageCache: {},
+  plugin: [watchify]
+});
+b.transform(babelify)
   .require('./src/main.js', { entry: true })
   .bundle()
   .on('error', function(err) {
@@ -22,8 +28,7 @@ var top = {
   js: './client/js/top.js',
   min: './client/js/top.min.js'
 };
-browserify({ debug: true })
-  .transform(babelify)
+b.transform(babelify)
   .require('./src/top.js', { entry: true })
   .bundle()
   .on('error', function(err) {
