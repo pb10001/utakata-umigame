4a446f0b91ef6e4fc7287f6f79b39e31306c9c61
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-06-25 16:17:48 +0900

    Fix bug in babel.js

--- babel.js
+++ babel.js
@@ -11,28 +11,7 @@ browserify({
   debug: true
 })
   .transform(babelify)
-  .transform(scssify, {
-    autoInject: true,
-    autoInject: 'verbose',
-    autoInject: {
-      verbose: false,
-      // If true the <style> tag will be prepended to the <head>
-      prepend: false
-    },
-    export: false,
-    sass: {
-      importer: 'custom-importers.js',
-      importerFactory: 'custom-importer-factory.js',
-      sourceMapEmbed: true,
-      sourceMapContents: true,
-      outputStyle: 'compressed'
-    },
-    postcss: {
-      autoprefixer: {
-        browsers: ['last 2 versions']
-      }
-    }
-  })
+  .transform(scssify)
   .require('./src/main.js', { entry: true })
   .bundle()
   .on('error', function(err) {
@@ -50,28 +29,7 @@ browserify({
   debug: true
 })
   .transform(babelify)
-  .transform(scssify, {
-    autoInject: true,
-    autoInject: 'verbose',
-    autoInject: {
-      verbose: false,
-      // If true the <style> tag will be prepended to the <head>
-      prepend: false
-    },
-    export: false,
-    sass: {
-      importer: 'custom-importers.js',
-      importerFactory: 'custom-importer-factory.js',
-      sourceMapEmbed: true,
-      sourceMapContents: true,
-      outputStyle: 'compressed'
-    },
-    postcss: {
-      autoprefixer: {
-        browsers: ['last 2 versions']
-      }
-    }
-  })
+  .transform(scssify)
   .require('./src/top.js', { entry: true })
   .bundle()
   .on('error', function(err) {
