e8f877cb70d3ef95ddfb718e948c88c762012184
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-06-28 17:44:15 +0900

    Update main.js

---
+++ client/main.js
@@ -0,0 +1,16 @@
+const electron = require("electron");
+const path = require('path');
+const url = require('url');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  //ビルド時はコメントアウト
+  mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+});
