d363880b2dbc2a11f663b485eea96e0e777719dc
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-06-28 17:44:24 +0900

    Update main.js

--- client/main.js
+++ client/main.js
@@ -7,6 +7,7 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow = null;
 app.on("ready", () => {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL('https://www.utakata-umigame.com');
 
   //ビルド時はコメントアウト
   mainWindow.webContents.openDevTools();
