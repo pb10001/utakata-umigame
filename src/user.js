7928f239083a109915519d920dc874e7a2ac8e17
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-08-24 06:30:56 +0000

    Employ Web Storage

--- src/user.js
+++ src/user.js
@@ -4,16 +4,20 @@ module.exports = function() {
   this.currentRoom = '';
   this.perPage = 10;
   this.getName = function() {
-    return this.name;
+    if(this.name) return this.name;
+    else return sessionStorage.name;
   };
   this.setName = function(name) {
     this.name = name;
+    sessionStorage.name = name;
   };
   this.getRemovePass = function() {
-    return this.removePass;
+    if(this.removePass) return this.removePass;
+    else return sessionStorage.removePass;
   };
   this.setRemovePass = function(removePass) {
     this.removePass = removePass;
+    sessionStorage.removePass = removePass;
   };
   this.getRoom = function() {
     return this.currentRoom;
@@ -22,9 +26,11 @@ module.exports = function() {
     this.currentRoom = room;
   };
   this.getPerPage = function() {
-    return this.perPage;
+    if(this.perPage) return this.perPage;
+    else return sessionStorage.perPage;
   };
   this.setPerPage = function(value) {
     this.perPage = value;
+    sessionStorage.perPage = value;
   };
 };
