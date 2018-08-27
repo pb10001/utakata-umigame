e3673c46e2c7603ac706e64f80b69daf3fa1d199
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-08-27 10:04:43 +0000

    Employ localStorage

--- src/user.js
+++ src/user.js
@@ -1,41 +1,43 @@
+/* global localStorage */
 module.exports = function() {
   this.name = '';
   this.removePass = '';
   this.currentRoom = '';
+  this.storage = localStorage;
   this.getName = function() {
     if(this.name) return this.name;
-    else if(sessionStorage.name) return sessionStorage.name;
+    else if(this.storage.name) return this.storage.name;
     else return '';
   };
   this.setName = function(name) {
     this.name = name;
-    sessionStorage.name = name;
+    this.storage.name = name;
   };
   this.getRemovePass = function() {
     if(this.removePass) return this.removePass;
-    else if(sessionStorage.removePass) return sessionStorage.removePass;
+    else if(this.storage.removePass) return this.storage.removePass;
     else return '';
   };
   this.setRemovePass = function(removePass) {
     this.removePass = removePass;
-    sessionStorage.removePass = removePass;
+    this.storage.removePass = removePass;
   };
   this.getRoom = function() {
     if(this.currentRoom) return this.currentRoom;
-    else if(sessionStorage.currentRoom) return sessionStorage.currentRoom;
+    else if(this.storage.currentRoom) return this.storage.currentRoom;
     else return '';
   };
   this.setRoom = function(room) {
     this.currentRoom = room;
-    sessionStorage.currentRoom = room;
+    this.storage.currentRoom = room;
   };
   this.getPerPage = function() {
     if(this.perPage) return this.perPage;
-    else if(sessionStorage.perPage) return sessionStorage.perPage;
+    else if(this.storage.perPage) return this.storage.perPage;
     else return 10;
   };
   this.setPerPage = function(value) {
     this.perPage = value;
-    sessionStorage.perPage = value;
+    this.storage.perPage = value;
   };
 };
