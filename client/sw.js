5aa9119a30b52b0b6610970053a572d514d2bf43
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-08-29 16:36:03 +0000

    Update sw.js

--- client/sw.js
+++ client/sw.js
@@ -1,5 +1,28 @@
 var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+// キャッシュにバージョンを付けておくと、古いキャッシュを消す時に便利
+var CACHE_STATIC_VERSION = 'static-v1';
 
+// サービスワーカーのインストール
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker...');
+
+  // キャッシュできるまで次の処理を待つ
+  event.waitUntil(
+    caches.open(CACHE_STATIC_VERSION)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App...');
+        // 何でもキャッシュできる。cssとかの中で更にリクエストが発生する場合は、動的にキャッシュする必要がある（後述）
+        cache.addAll([
+          '/',
+          '/mondai',
+          '/template.html',
+          '/mondai_beta.html',
+          '/lobby_chat.html',
+          '/chat_box.html',
+        ]);
+      })
+  );
+});
 self.addEventListener('fetch', function(event) {
   console.log('[Service Worker] Fetching something ...');
   event.respondWith(
@@ -25,4 +48,19 @@ self.addEventListener('fetch', function(event) {
         }
       })
   );
+});
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker...');
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_VERSION && key !== CACHE_DYNAMIC_VERSION) {
+            console.log('[Service Worker] Removing old cache...');
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
 });
