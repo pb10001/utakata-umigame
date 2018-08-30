2f3811b507d8700a2b7d2f1228c4d3bf7444454b
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-08-30 08:17:50 +0000

    Update the version

--- client/sw.js
+++ client/sw.js
@@ -1,7 +1,7 @@
 /* global caches */
-var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+var CACHE_DYNAMIC_VERSION = 'dynamic-v2.1.1';
 // キャッシュにバージョンを付けておくと、古いキャッシュを消す時に便利
-var CACHE_STATIC_VERSION = 'static-v11';
+var CACHE_STATIC_VERSION = 'static-v2.1.1';
 
 // サービスワーカーのインストール
 self.addEventListener('install', function(event) {
